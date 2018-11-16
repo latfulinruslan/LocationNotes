fef1c4a3d0335a2115b1943bcf153c444bad0ec1
Ruslan <latfulinr@gmail.com>
2018-11-16 11:38:53 +0300

    Initial Commit

---
+++ LocationNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationNotes
+//
+//  Created by Ruslan on 16.11.2018.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
